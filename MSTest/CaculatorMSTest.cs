a15aa62d0347a0d131f3555a5d650893e3fd98e5
Tung Dao <daotung492@gmail.com>
2023-09-06 06:17:35 +0700

    Add project files.

---
+++ MSTest/CaculatorMSTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTest;
+
+namespace MSTest
+{
+	[TestClass]
+	public class CaculatorMSTest
+	{
+		[TestMethod]
+		public void AddNumbers_InputTwoInt_GetCorrectAddition()
+		{
+			// 3 giai doan
+			// arrange phase : khoi tao
+			Caculator calc = new Caculator();
+
+
+
+			// act phase : action
+			int result = calc.AddNumbers(1, 2);
+
+			// assert phase : check result
+			Assert.AreEqual(3, result);
+		}
+	}
+}
---
+++ UnitTest/Caculator.cs
@@ -0,0 +1,11 @@
+﻿namespace UnitTest
+{
+	public class Caculator
+	{
+		public int AddNumbers(int a, int b)
+		{
+			return a + b;
+		}
+
+	}
+}
